dac8a2ed0b8b14d0708985cf392cda020a304e6f
Дмитрий Куриленко < >
2016-07-06 12:32:19 +0300

    new Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,19 @@
 public class Calculate{
+
     public static void main(String[] arg){
-         System.out.println("Calculate"); 
+
+        System.out.println("Calculate");
+        int first = Integer.valueOf(arg[0]);
+        int second = Integer.valueOf(arg[1]);
+        int summ = first + second;
+        System.out.println("sum = " + summ);
+        int mno = first * second;
+        System.out.println("mno = " + mno);
+        int vid = first - second;
+        System.out.println("vid = " + vid);
+        double dill = (first/second);
+        System.out.println("dil = " + dill);
+        long step = (first^second);
+        System.out.println("step = " + step);
   }
 }
